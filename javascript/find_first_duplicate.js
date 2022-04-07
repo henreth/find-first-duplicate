5e4836f7d0c9b5eb3d03db744ddb7aa3a6cbf573
Henry Miller <mail.henrym+git@gmail.com>
2022-04-07 11:28:13 -0400

    finished both

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -1,5 +1,14 @@
 function findFirstDuplicate(arr) {
-  // type your code here
+  let newArr =[];
+  for (let i = 0; i < arr.length; i++){
+    if (newArr.includes(arr[i])){
+      return arr[i] 
+    } else {
+      newArr.push(arr[i])
+    }
+  }
+    return -1
+
 }
 
 if (require.main === module) {
